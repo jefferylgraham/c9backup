e878199bb4235b9bbcebbe2815f4337646679806
Jeff Graham <jeffery.lemar.graham@gmail.com>
2019-06-25 23:39:56 +0000

    add pricing

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -19,13 +19,14 @@ router.get("/", function(req, res) {
 //CREATE ROUTE - add new campgrounds to db
 router.post("/", middleware.isLoggedIn, function(req, res) {
    var name = req.body.name;
+	var price = req.body.price;
    var image = req.body.image;
    var description = req.body.description;
    var author = {
        id: req.user._id,
        username: req.user.username
    };
-   var newCampground = {name: name, image: image, description: description, author: author};
+   var newCampground = {name: name, price: price, image: image, description: description, author: author};
    
    //create a new campground in db
    Campground.create(newCampground, function(err, newlyCreated) {
